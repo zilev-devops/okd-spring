9c5312131c8fa303c25429f944aceb0ca469df62
zilev <zilev.dev@gmail.com>
2024-09-05 09:58:37 -0500

    Hello World

---
+++ src/main/java/pe/gob/congreso/okd/demo/controller/HomeController.java
@@ -0,0 +1,12 @@
+package pe.gob.congreso.okd.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String index() {
+        return "Hola desde OKD";
+    }
+}
---
+++ src/test/java/pe/gob/congreso/okd/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.congreso.okd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
